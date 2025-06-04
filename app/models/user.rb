f0013dad64c21bef40adb10f99e5b66aeecc4ce9
Arunava Das <arunava52486@gmail.com>
2025-06-04 20:39:51 +0530

    part 1 done: imported all necessary validations & test cases from old project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,59 @@
 class User < ApplicationRecord
     has_secure_password
+
+    before_validation :strip_whitespace, :normalize_email
+
+    validates :first_name, :last_name, :email, :date_of_birth, :country, presence: true
+
+    validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "must contain only alphabets" }
+    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :country, numericality: { only_integer: true, message: "must be a whole number" }
+    
+    # validates :country, format: { with: /........../, message: "........" }, inclusion: { in: COUNTRIES, message: "is not in our country list" }
+
+    
+    validate :validate_date_of_birth
+
+    private
+
+    def normalize_email
+        self.email = email.downcase if email.present?
+    end
+
+    def strip_whitespace
+        self.first_name = first_name.strip if first_name.present?
+        self.last_name = last_name.strip if last_name.present?
+        self.email = email.strip if email.present?
+        # self.password = password.strip if password.present?
+    end
+
+    def validate_date_of_birth
+        return if date_of_birth.blank?
+
+        unless date_of_birth.is_a?(Date)
+            errors.add(:date_of_birth, "must be a valid date")
+        end
+        # need to revisit this validation later
+
+        if date_of_birth > Date.current
+            errors.add(:date_of_birth, "can not be today or a future date")
+        end
+    end
+
+    # def validate_email_domain
+    #     return if email.blank?
+
+    #     VALID_EMAIL_DOMAINS.each do |domain|
+    #         return if email.include?(domain)
+    #     end
+
+    #     errors.add(:email, "domain is not supported")
+    # end
+
+    # def create_fixed_lists
+    #     FixedList.create(user: self, name: "WatchList")
+    #     FixedList.create(user: self, name: "Watched")
+    #     FixedList.create(user: self, name: "Favourite Movies")
+    #     FixedList.create(user: self, name: "Favourite TV Shows")
+    # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,132 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'should have all the attributes' do
+  let(:valid_attributes) do
+    {
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@gmail.com',
+      password: '1234',
+      date_of_birth: Date.new(1990, 12, 28),
+      country: 4
+    }
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      user = User.new(valid_attributes)
+      expect(user).to be_valid
+    end
+
+    context 'first_name validations' do
+      it 'can not be empty' do
+        user = User.new(valid_attributes.merge(first_name: nil))
+        expect(user).not_to be_valid
+        expect(user.errors[:first_name]).to include("can't be blank")
+      end
+
+      it 'must contain only alphabets' do
+        user = User.new(valid_attributes.merge(first_name: 'John123'))
+        expect(user).not_to be_valid
+        expect(user.errors[:first_name]).to include('must contain only alphabets')
+      end
+    end
+
+    context 'last_name validations' do
+      it 'can not be empty' do
+        user = User.new(valid_attributes.merge(last_name: nil))
+        expect(user).not_to be_valid
+        expect(user.errors[:last_name]).to include("can't be blank")
+      end
+
+      it 'must contain only alphabets' do
+        user = User.new(valid_attributes.merge(last_name: 'Doe123'))
+        expect(user).not_to be_valid
+        expect(user.errors[:last_name]).to include('must contain only alphabets')
+      end
+    end
+
+    context 'email validations' do
+      it 'is not valid without an email' do
+        user = User.new(valid_attributes.merge(email: nil))
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+
+      it 'is not valid with an invalid email format' do
+        user = User.new(valid_attributes.merge(email: 'invalid-email'))
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include('is invalid')
+      end
+
+      it 'is not valid with a duplicate email' do
+        User.create!(valid_attributes)
+        user = User.new(valid_attributes)
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include('has already been taken')
+      end
+
+      # it 'must have a supported email domain' do
+      #   user = User.new(valid_attributes.merge(email: 'arunava.das@example.com'))
+      #   expect(user).not_to be_valid
+      #   expect(user.errors[:email]).to include('domain is not supported')
+      # end
+    end
+
+    context 'date_of_birth validations' do
+      it 'is not valid without a date_of_birth' do
+        user = User.new(valid_attributes.merge(date_of_birth: nil))
+        expect(user).not_to be_valid
+        expect(user.errors[:date_of_birth]).to include("can't be blank")
+      end
+
+      it 'date_of_birth must be a valid date' do
+        user = User.new(valid_attributes.merge(date_of_birth: 'abcd-xy-00'))
+        expect(user).not_to be_valid
+      end
+
+      it 'date_of_birth cant be a future date' do
+        user = User.new(valid_attributes.merge(date_of_birth: Date.current + 1.day))
+        expect(user).not_to be_valid
+        expect(user.errors[:date_of_birth]).to include('can not be today or a future date')
+      end
+    end
+
+    context 'country validations' do
+      it 'must be present' do
+        user = User.new(valid_attributes.merge(country: nil))
+        expect(user).not_to be_valid
+        expect(user.errors[:country]).to include("can't be blank")
+      end
+
+      it 'must be a number' do
+        user = User.new(valid_attributes.merge(country: 'canada'))
+        puts "-------------------"
+        puts user.inspect
+        puts user.valid?
+        puts user.errors
+        # expect(user.errors[:country]).to include('must contain only ')
+        expect(user).not_to be_valid
+      end
+
+      # it 'must be in our country list' do
+      #   user = User.new(valid_attributes.merge(country: 'Tiwan'))
+      #   expect(user).not_to be_valid
+      #   expect(user.errors[:country]).to include('is not in our country list')
+      # end
+    end
+
+  context 'associations' do
+    # Add association tests here if you have any associations in the future
+  end
+
+  context 'scopes' do
+    # Add scope tests here if you have any scopes in the future
+  end
+
+  context 'methods' do
+    # Add method tests here if you have any custom methods in the future
+  end
+
   end
 end
