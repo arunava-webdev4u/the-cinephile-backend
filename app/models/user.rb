5c38acc413374bf31a8e0b7a573b09883925bd01
Arunava Das <arunava52486@gmail.com>
2025-06-05 21:57:31 +0530

    part 2 done: implemented most important validations and added their specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,43 @@
 class User < ApplicationRecord
     has_secure_password
 
-    before_validation :strip_whitespace, :normalize_email
+    before_validation :strip_whitespace
 
-    validates :first_name, :last_name, :email, :date_of_birth, :country, presence: true
+    validates :first_name, :last_name, :email, :date_of_birth, :country,
+        presence: true
 
-    validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "must contain only alphabets" }
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :country, numericality: { only_integer: true, message: "must be a whole number" }
-    
+    validates :first_name, :last_name,
+        length: { maximum: 50, minimum: 1 },
+        format: { with: /\A[a-zA-Z]+\z/, message: "must contain only alphabets" }
+
+    validates :email,
+        length: { maximum: 254 },
+        uniqueness: { case_sensitive: false },
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+
+    validates :country,
+        numericality: { only_integer: true, greater_than: 0 }
+
     # validates :country, format: { with: /........../, message: "........" }, inclusion: { in: COUNTRIES, message: "is not in our country list" }
 
-    
     validate :validate_date_of_birth
 
-    private
+    def age
+        ((Date.current - date_of_birth) / 365.25).floor
+    end
 
-    def normalize_email
-        self.email = email.downcase if email.present?
+    def full_name
+        (self.first_name + " " + self.last_name).strip
+    end
+
+    def adult?
+        age && age >= 18
     end
 
     def strip_whitespace
         self.first_name = first_name.strip if first_name.present?
         self.last_name = last_name.strip if last_name.present?
         self.email = email.strip if email.present?
-        # self.password = password.strip if password.present?
     end
 
     def validate_date_of_birth
@@ -32,28 +45,34 @@ class User < ApplicationRecord
 
         unless date_of_birth.is_a?(Date)
             errors.add(:date_of_birth, "must be a valid date")
+            return
         end
         # need to revisit this validation later
 
         if date_of_birth > Date.current
             errors.add(:date_of_birth, "can not be today or a future date")
+            return
+        end
+
+        if date_of_birth < 120.years.ago
+            errors.add(:date_of_birth, "must be within the last 120 years")
         end
     end
 
-    # def validate_email_domain
-    #     return if email.blank?
+  # def validate_email_domain
+  #     return if email.blank?
 
-    #     VALID_EMAIL_DOMAINS.each do |domain|
-    #         return if email.include?(domain)
-    #     end
+  #     VALID_EMAIL_DOMAINS.each do |domain|
+  #         return if email.include?(domain)
+  #     end
 
-    #     errors.add(:email, "domain is not supported")
-    # end
+  #     errors.add(:email, "domain is not supported")
+  # end
 
-    # def create_fixed_lists
-    #     FixedList.create(user: self, name: "WatchList")
-    #     FixedList.create(user: self, name: "Watched")
-    #     FixedList.create(user: self, name: "Favourite Movies")
-    #     FixedList.create(user: self, name: "Favourite TV Shows")
-    # end
+  # def create_fixed_lists
+  #     FixedList.create(user: self, name: "WatchList")
+  #     FixedList.create(user: self, name: "Watched")
+  #     FixedList.create(user: self, name: "Favourite Movies")
+  #     FixedList.create(user: self, name: "Favourite TV Shows")
+  # end
 end
