904ff5cdec7c2ad210e1c9f3741a2a56517b8b78
Arunava Das <arunava52486@gmail.com>
2025-06-19 23:40:05 +0530

    added list functionality (for default list only for now)

---
+++ app/models/default_list.rb
@@ -0,0 +1,37 @@
+class DefaultList < List
+    validates :name, inclusion: {
+        in: [ "watchlist", "watched", "favourite_movies", "favourite_tv_Shows" ],
+        message: "must be one of the predefined default list names"
+    }
+
+    def can_be_deleted?
+        true
+    end
+
+    def can_be_updated?
+        true
+    end
+
+    # Override methods to prevent any kind of Write operation
+    def destroy
+        false
+    end
+    def destroy!
+        raise ActiveRecord::RecordNotDestroyed, "Default lists cannot be deleted"
+    end
+    def update(attributes)
+        false
+    end
+    def update!(attributes)
+        raise ActiveRecord::RecordNotSaved, "Default lists cannot be updated"
+    end
+    def update_attribute(name, value)
+        false
+    end
+    def update_attributes(attributes)
+        false
+    end
+    def update_attributes!(attributes)
+        raise ActiveRecord::RecordNotSaved, "Default lists cannot be updated"
+    end
+end
