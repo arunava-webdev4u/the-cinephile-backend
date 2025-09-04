1b914b40dec5b0427c87017b5bcb3cc646f94624
Arunava Das <arunava52486@gmail.com>
2025-09-05 01:09:44 +0530

    rubocop

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Authenticable, type: :concern  do
     let(:dummy_controller_class) do
         Class.new(ApplicationController) do
             include Authenticable
-            
+
             def test_action
                 render json: { message: 'success' }
             end
