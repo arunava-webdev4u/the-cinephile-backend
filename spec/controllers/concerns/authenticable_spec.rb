d881b3b5275979225618e4f7a0a58fc79a566dc8
Arunava Das <arunava52486@gmail.com>
2025-09-08 01:24:23 +0530

    spec initialized, and running

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -1,13 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe Authenticable, type: :concern  do
-    let(:dummy_controller_class) do
-        Class.new(ApplicationController) do
-            include Authenticable
+RSpec.describe Authenticable, type: :controller  do
+    controller(Api::V1::ApplicationController) do
+        include Authenticable
+        
+        def index
+            render json: { message: "success" }
+        end
+    end
 
-            def test_action
-                render json: { message: 'success' }
+    let(:jti) { SecureRandom.uuid }
+    let(:user) { create(:user, jti: jti) }
+    let(:token) { Auth::JsonWebToken.encode(user_id: user.id, jti: jti) }
+
+    before do
+        path = controller.controller_path
+        routes.draw { get "index" => "#{path}#index" }
+    end
+
+    describe "GET #index" do
+        context "when Authorization header is missing" do
+            it "returns unauthorized" do
+                get :index
+
+                expect(response).to have_http_status(:unauthorized)
+                expect(JSON.parse(response.body)["error"]).to eq("Authorization token is missing")
             end
         end
+
+        # context "when Authorization header is present but invalid format" do
+            # it "returns unauthorized" do
+        
+        # context "when token is invalid or expired" do
+        #     before do
+        #     allow(Auth::JsonWebToken).to receive(:decode).and_return(nil)
+        #   end
+            #  it "returns unauthorized" do
+
+        # context "when decoded token does not match any user" do
+            # it "returns unauthorized" do
+            
+        #  context "when decoded token jti does not match user jti" do
+            # it "returns unauthorized" do
+
+        #  context "when token is valid" do
     end
 end
