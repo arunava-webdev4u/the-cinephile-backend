a12eaf0ee5127cbd505d96107fa0c8be6dc11e8b
Arunava Das <arunava52486@gmail.com>
2025-09-08 23:22:52 +0530

    testing done for concern

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -28,21 +28,71 @@ RSpec.describe Authenticable, type: :controller  do
             end
         end
 
-        # context "when Authorization header is present but invalid format" do
-            # it "returns unauthorized" do
+        context "when Authorization header is present but invalid format" do
+            it "returns unauthorized" do
+                request.headers["Authorization"] = "InvalidTokenFormat"
+                get :index
+
+                expect(response).to have_http_status(:unauthorized)
+                expect(JSON.parse(response.body)["error"]).to eq("Authorization token is missing")
+            end
+        end
         
-        # context "when token is invalid or expired" do
-        #     before do
-        #     allow(Auth::JsonWebToken).to receive(:decode).and_return(nil)
-        #   end
-            #  it "returns unauthorized" do
+        context "when token is invalid or expired" do
+            before do
+                allow(Auth::JsonWebToken).to receive(:decode).and_return(nil)
+            end
 
-        # context "when decoded token does not match any user" do
-            # it "returns unauthorized" do
-            
-        #  context "when decoded token jti does not match user jti" do
-            # it "returns unauthorized" do
+            it "returns unauthorized" do
+                request.headers["Authorization"] = "Bearer invalid-or-expired-token"
+                get :index
 
-        #  context "when token is valid" do
+                expect(response).to have_http_status(:unauthorized)
+                expect(JSON.parse(response.body)["error"]).to eq("Invalid or expired token")
+            end
+        end
+
+        context "when decoded token does not match any user" do
+            before do
+                allow(User).to receive(:find_by).and_return(nil)
+            end
+
+            it "returns unauthorized" do
+                request.headers["Authorization"] = "Bearer #{token}"
+                get :index
+
+                expect(response).to have_http_status(:unauthorized)
+                expect(JSON.parse(response.body)["error"]).to eq("Invalid or expired token")
+            end
+        end
+
+        context "when decoded token jti does not match user jti" do
+            before do
+                allow(Auth::JsonWebToken).to receive(:decode).and_return({ user_id: user.id, jti: "different-jti" })
+            end
+
+            it "returns unauthorized" do
+                request.headers["Authorization"] = "Bearer #{token}"
+                get :index
+
+                expect(response).to have_http_status(:unauthorized)
+                expect(JSON.parse(response.body)["error"]).to eq("Invalid or expired token")
+            end
+        end
+        
+        context "when token is valid" do
+            before do
+                allow(Auth::JsonWebToken).to receive(:decode).and_return({ user_id: user.id, jti: user.jti })
+            end
+
+            it "sets @current_user and allows access" do
+                request.headers["Authorization"] = "Bearer #{token}"
+                get :index
+                
+                expect(response).to have_http_status(:ok)
+                expect(JSON.parse(response.body)).to eq({ "message" => "success" })
+                expect(controller.instance_variable_get(:@current_user)).to eq(user)
+            end
+        end
     end
 end
