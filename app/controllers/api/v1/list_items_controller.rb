b74057fdd0c06f97ea1da6406723e6cac8f41611
Arunava Das <arunava52486@gmail.com>
2025-09-24 01:30:52 +0530

    list item controller is added

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -2,7 +2,23 @@ class Api::V1::ListItemsController < Api::V1::ApplicationController
     before_action :set_list
 
     def index
-        render json: { list_items: @list.list_items }, status: :ok
+        render json: @list.list_items, status: :ok
+    end
+
+    def create
+        list_item = @list.list_items.new(list_item_params)
+        
+        if list_item.save
+            render json: list_item, status: :created
+        else
+            render json: { errors: list_item.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        list_item = @list.list_items.find(params[:id])
+        list_item.destroy
+        render json: { message: "Item removed from list" }, status: :ok
     end
 
     private
@@ -12,5 +28,9 @@ class Api::V1::ListItemsController < Api::V1::ApplicationController
         elsif params[:type] == "DefaultList"
             @list = DefaultList.find(params[:custom_list_id])
         end
+    end
+
+    def list_item_params
+        params.require(:list_item).permit(:item_id, :item_type)
     end
 end
