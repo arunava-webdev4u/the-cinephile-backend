c87367a54b674a311ccae18143d28450341ae0e2
Arunava Das <arunava52486@gmail.com>
2026-04-13 23:04:32 +0530

    optimize register-background job flow

--- app/workers/send_verification_email_worker.rb
+++ app/workers/send_verification_email_worker.rb
@@ -3,9 +3,14 @@ class SendVerificationEmailWorker
 
   sidekiq_options queue: :mailers, retry: 3
 
-  def perform(registration_details)
-    # Sidekiq serializes args as JSON, so symbol keys become string keys.
-    # deep_symbolize_keys restores the expected format for SmtpGmailService.
-    SmtpGmailService.new.send_verification_email(registration_details.deep_symbolize_keys)
+  def perform(user_id)
+    user = User.find(user_id)
+    registration_details = {
+      email:      user.email,
+      first_name: user.first_name,
+      last_name:  user.last_name,
+      otp_code:   user.verification.otp_code
+    }
+    SmtpGmailService.new.send_verification_email(registration_details)
   end
 end
