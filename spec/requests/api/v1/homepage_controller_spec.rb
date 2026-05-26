c754f7f2ad3103c6cb74d4976b5806dacd28c10c
Arunava Das <arunava52486@gmail.com>
2026-05-26 17:09:17 +0530

    added specs for homepage

--- spec/requests/api/v1/homepage_controller_spec.rb
+++ spec/requests/api/v1/homepage_controller_spec.rb
@@ -2,11 +2,26 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::HomepageController", type: :request do
     describe "#index" do
-        it "returns unauthorized" do
+        it "returns a welcome message with PostgreSQL version" do
             get "/"
 
             expect(response).to have_http_status(:ok)
-            expect(JSON.parse(response.body)["message"]).to start_with("Welcome to The Cinephile API.")
+            response_body = JSON.parse(response.body)
+            expect(response_body["message"]).to start_with("Welcome to The Cinephile API. PG version:")
+            expect(response_body["message"]).to match(/PG version: PostgreSQL \d+\.\d+/)
+        end
+
+        it "returns a welcome message even with an authentication header" do
+            user = FactoryBot.create(:user)
+            auth_token = "valid-token"
+            allow(Auth::JsonWebToken).to receive(:decode).and_return({ user_id: user.id })
+            headers = { "Authorization" => "Bearer #{auth_token}" }
+
+            get "/", headers: headers
+
+            expect(response).to have_http_status(:ok)
+            response_body = JSON.parse(response.body)
+            expect(response_body["message"]).to start_with("Welcome to The Cinephile API. PG version:")
         end
     end
 end
